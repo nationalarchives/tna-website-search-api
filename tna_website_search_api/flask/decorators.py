19a06e78030bacc4155464789d0e53d67c52885b
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2026-09-24 17:03:11 +0100

    Tidy up params

--- tna_website_search_api/flask/decorators.py
+++ tna_website_search_api/flask/decorators.py
@@ -1,4 +1,4 @@
-def searchable(view, title: str, description: str):
+def searchable(view, *, title: str | None = None, description: str | None = None) -> callable: 
     """
     Flask decorator to denote a view/route as "searchable", which will expose it to the API.
     """
